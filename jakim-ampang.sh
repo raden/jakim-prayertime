4ea9c20456a5d308c899e6563788aefb7a03d4db
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2017-06-15 13:51:54 +0800

    Added base file

---
+++ jakim-ampang.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+my_array=( $(lynx -dump http://www2.e-solat.gov.my/xml/today/?zon=SGR01|\
+cut --complement -d "," -f 1,3,4|\
+grep -v H.S|cut --complement -d "M" -f 1) )
+
+arrayLen=${#my_array[@]}
+arrayWaq=("Imsak" "Subuh" "Syuruk" "Zuhur" "Asar" "Maghrib" "Isyak")
+
+echo '--------------------------'
+echo -n
+echo 'WAKTU SOLAT BAGI AMPANG'
+echo '--------------------------'
+for ((i=2;i<${arrayLen};i++)); do echo "${arrayWaq[$i-2]}, ${my_array[$i]}"; done
+echo -n
+echo '--------------------------'
