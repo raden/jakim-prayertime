e4162cd45744f767c5bd81a68bfcc249f63d1a44
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2017-06-20 05:44:45 +0800

    Updated title

--- xmlstartlet-jakim.sh
+++ xmlstartlet-jakim.sh
@@ -1,1 +1,11 @@
-xmlstarlet sel --net -t -m "//item" -o "Waktu: " -v "title" -o " -- " -o "Masa: " -v "description" -n  http://www2.e-solat.gov.my/xml/today/?zon=SGR01
+xmlstarlet sel --net -t -n \
+-o "------------------------------" -n \
+-o "Kawasan: " \
+-m "//channel" -v "link" -n  \
+-o "Tarikh: " \
+-m "//channel" -v "dc:date" -n  \
+-o "------------------------------" -n \
+-t -m "//item" -o "Waktu: " -v "title" \
+ -o " -- " \
+-o "Masa: " -v "description" -n  \
+http://www2.e-solat.gov.my/xml/today/?zon=SGR01
