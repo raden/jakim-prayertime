3857a1045558615d4a38d1a1f5336e774411490f
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2017-06-20 05:45:04 +0800

    New file for xmlstarlet, with argument

---
+++ xmlstartlet-jakim-with-args.sh
@@ -0,0 +1,11 @@
+xmlstarlet sel --net -t -n \
+-o "------------------------------" -n \
+-o "Kawasan: " \
+-m "//channel" -v "link" -n  \
+-o "Tarikh: " \
+-m "//channel" -v "dc:date" -n  \
+-o "------------------------------" -n \
+-t -m "//item" -o "Waktu: " -v "title" \
+ -o " -- " \
+-o "Masa: " -v "description" -n  \
+http://www2.e-solat.gov.my/xml/today/?zon=$1
