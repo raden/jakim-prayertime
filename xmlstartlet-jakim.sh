f9450c0f33b26b379ced797ba892440c2040d0df
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2019-11-16 21:02:03 +0800

    Letak peringatan untuk letak kod kawasan

--- xmlstartlet-jakim.sh
+++ xmlstartlet-jakim.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-lynx -source "https://www.e-solat.gov.my/index.php?r=esolatApi/xmlfeed&zon=$1" > /tmp/solat.xml
+
+SASARAN=/tmp/solat.xml
+
+if [[ -z $1 ]]; then
+echo "Sila letak kod lokasi"
+echo "Cara guna, $0 <kod lokasi>"
+echo -n
+exit
+fi
+
+lynx -source "https://www.e-solat.gov.my/index.php?r=esolatApi/xmlfeed&zon=$1" > $SASARAN
 xmlstarlet sel -T -t -n \
 -o "------------------------------" -n \
 -o "Kawasan: " \
@@ -10,4 +20,4 @@ xmlstarlet sel -T -t -n \
 -t -m "//item" -o "Waktu: " -v "title" \
 -o " -- " \
 -o "Masa: " -v "description" -n  \
-/tmp/solat.xml
+$SASARAN
